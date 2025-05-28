d4f9e1571f3a4437750b1b9415f382f9771fc56b
Graysen Gould <graysen.gould@tylertech.com>
2025-05-28 17:35:36 -0500

    angular pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,14 @@ export class AppComponent {
 
     courses = COURSES;
 
+    title = COURSES[0].description;
+    
+    price: number  = 9.999;
+
+    rate =  0.67;
+
+    startDate = new Date(2000, 0, 1);
+
     onCourseSelected(course: Course) {
         console.log("App component - click event bubbled ...",course);
     }
@@ -20,6 +28,5 @@ export class AppComponent {
     trackCourse(index: number, course: Course){ // is this correct?
         return course.id;
     }
-
 
 }
