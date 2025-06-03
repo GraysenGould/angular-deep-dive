1b7bb23ed845e4214fa9b7a1c18d8f396ffc5371
Graysen Gould <graysen.gould@tylertech.com>
2025-06-02 21:13:30 -0500

    child decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef , AfterViewInit} from '@angular/core';
+import { Component, ViewChild, ElementRef , AfterViewInit, ViewChildren, QueryList} from '@angular/core';
 import {COURSES, findCourseById} from '../db-data';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {Course} from "./model/course";
@@ -13,24 +13,36 @@ export class AppComponent implements AfterViewInit{
  
     courses = COURSES;
 
-    @ViewChild('cardRef1', {read: ElementRef})
-    card1: ElementRef; 
+    // @ViewChild('cardRef1', {read: ElementRef})
+    // card1: ElementRef; 
+    @ViewChildren (CourseCardComponent, {read: ElementRef}) 
+    cards: QueryList<ElementRef>;
 
-
-    @ViewChild('courseImage')
-    courseImage: ElementRef;
-
-    constructor () { 
-            console.log("containerDiv", this.card1);
-    }
 
     ngAfterViewInit () {
-        console.log("courseImage", this.courseImage);
+        // this.cards.changes.subscribe(
+        //     cards => console.log(cards)
+        // );
+        // console.log("cards:", this.cards.first);
+        console.log(this.cards);
+        
+        
+    }
+
+    onCoursesEdited () {
+        this.courses.push(    {
+        id: 1,
+        description: "Angular core deep dive",
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+        category: 'INTERMEDIATE',
+        lessonsCount: 10
+        }
+    );
     }
 
     onCourseSelected(course: Course) {
         console.log("App component - click event bubbled ...",course);
-        console.log("card1", this.card1); 
     }
 
 }
